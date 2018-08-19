8ece182bc784bed29d012bf49d60b9b97ea93c16
GamesWorth <s3691198@student.rmit.edu.au>
2018-08-19 17:56:58 +1000

    Finilised implimentation of webserver, with graph code from source

---
+++ webserver.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+
+#code written using both the weekly pracs and from https://www.instructables.com/id/From-Data-to-Graph-a-Web-Jorney-With-Flask-and-SQL/
+#by Aidan Harris
+
+'''Uncomment if you dont want to see console print out'''
+#import logging
+#log = logging.getLogger('werkzeug')
+#log.setLevel(logging.ERROR)
+
+# Retrieve data from database
+from datetime import datetime
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import io
+
+from flask import Flask, render_template, send_file, make_response, request
+app = Flask(__name__)
+import os
+import sqlite3
+
+database = '/home/pi/Assignment_1/sensehat.db'
+conn=sqlite3.connect(database)
+curs=conn.cursor()
+
+# Retrieve LAST data from database
+def getLastData():
+	for row in curs.execute("SELECT * FROM SENSEHAT_data ORDER BY timestamp DESC LIMIT 1"):
+		time = str(row[0])
+		temp = row[1]
+		hum = row[2]
+	#conn.close()
+	return time, temp, hum
+
+# Get 'x' samples of historical data
+def getHistData (numSamples):
+	curs.execute("SELECT * FROM SENSEHAT_data ORDER BY timestamp DESC LIMIT "+str(numSamples))
+	data = curs.fetchall()
+	dates = []
+	temps = []
+	hums = []
+	for row in reversed(data):
+		dates.append(row[0])
+		temps.append(row[1])
+		hums.append(row[2])
+		temps, hums = testeData(temps, hums)
+	return dates, temps, hums
+
+# Test data for cleanning possible values not in range
+def testeData(temps, hums):
+	n = len(temps)
+	for i in range(0, n-1):
+		if (temps[i] < -10 or temps[i] >50):
+			temps[i] = temps[i-2]
+		if (hums[i] < 0 or hums[i] >100):
+			hums[i] = temps[i-2]
+	return temps, hums
+
+
+# Get Max number of rows (table size)
+def maxRowsTable():
+	for row in curs.execute("select COUNT(temp) from SENSEHAT_data"):
+		maxNumberRows=row[0]
+	return maxNumberRows
+
+# Get sample frequency in minutes
+def freqSample():
+	return (10)
+
+# define and initialize global variables
+global numSamples
+numSamples = maxRowsTable()
+if (numSamples > 101):
+        numSamples = 100
+
+global freqSamples
+freqSamples = freqSample()
+
+global rangeTime
+rangeTime = 100
+				
+		
+# main route 
+@app.route("/")
+def index():
+	time, temp, hum = getLastData()
+	templateData = {
+	  'time'		: time,
+      'temp'		: temp,
+      'hum'			: hum,
+      'freq'		: freqSamples,
+      'rangeTime'		: rangeTime
+	}
+	return render_template('index.html', **templateData)
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    global numSamples 
+    global freqSamples
+    global rangeTime
+    rangeTime = int (request.form['rangeTime'])
+    if (rangeTime < freqSamples):
+        rangeTime = freqSamples + 1
+    numSamples = rangeTime//freqSamples
+    numMaxSamples = maxRowsTable()
+    if (numSamples > numMaxSamples):
+        numSamples = (numMaxSamples-1)
+    
+    time, temp, hum = getLastData()
+    
+    templateData = {
+	  'time'		: time,
+      'temp'		: temp,
+      'hum'			: hum,
+      'freq'		: freqSamples,
+      'rangeTime'	: rangeTime
+	}
+    return render_template('index.html', **templateData)
+	
+	
+@app.route('/plot/temp')
+def plot_temp():
+	times, temps, hums = getHistData(numSamples)
+	ys = temps
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("Temperature [°C]")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(numSamples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
+
+@app.route('/plot/hum')
+def plot_hum():
+	times, temps, hums = getHistData(numSamples)
+	ys = hums
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("Humidity [%]")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(numSamples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
+	
+if __name__ == "__main__":
+   host = os.popen('hostname -I').read()
+   app.run(host=host, port=80, debug=False)
